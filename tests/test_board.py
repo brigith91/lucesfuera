f6a2bfc7334665238419706a3ffd51458fabc3cb
brigith91 <168245239+brigith91@users.noreply.github.com>
2025-04-15 20:20:30 +0000

    cambios

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,89 +1,109 @@
-from lucesfuera.game.board import Posicion
-import sys
-import os
-sys.path.insert(0, os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..')))
+from lucesfuera.game.board import Tablero, Posicion
+from lucesfuera.game.luz import Luz
+import pytest
 
 
+@pytest.mark.parametrize(
+    "fila, col_init, col_fin",
+    [
+        (0, 0, 1) # 1
+        (0, -5, -4) # 2
+        (0, 100, 101) # 3
+    ]
+)
 
+def test_posicion_deberia_mover_derecha(fila, col_init, col_fin):
+    p= Posicion(fila, col_init)
+    resultado = Posicion =p.new_posicion_derecha()
+    esperado: Posicion = Posicion(fila,col_fin) 
+    assert resultado == esperado
 
-def test_mover_derecha():
-    posicion_inicial = Posicion(2, 3)
-    nueva_posicion = posicion_inicial.new_posicion_derecha()
-    assert nueva_posicion.fila == 2
-    assert nueva_posicion.columna == 4
-    assert (nueva_posicion.fila, nueva_posicion.columna) == (2, 4)
+@pytest.mark.parametrize(
+    "pos_inicial, pos_final"
+    [
+        ((0, 0),   (0, -1)),       # 1
+        ((0, -5),  (0, -6)),       # 2
+        ((0, 100), (0, 99)),       # 3
+        ((-5, 0),  (-5, -1)),      # 4
+        ((-5, -5), (-5, -6)),      # 5
+        ((-5, -100), (-5, 99)),    # 6
+    ]
+)
 
+def test_posicion_deberia_mover_izquierda(pos_inicial, pos_final):
+    p= Posicion(pos_inicial, [0],pos_inicial[1] )
+    resultado = Posicion =p.new_posicion_izquierda()
+    esperado: Posicion = Posicion(pos_final[0], pos_final[1]) # type: ignore
+    assert resultado == esperado
 
-def test_mover_izquierda():
-    posicion_inicial = Posicion(2, 3)
-    nueva_posicion = posicion_inicial.new_posicion_izquierda()
-    assert nueva_posicion.fila == 2
-    assert nueva_posicion.columna == 2
-    assert (nueva_posicion.fila, nueva_posicion.columna) == (2, 2)
+@pytest.mark.skip("no se ha implementado el movimiento diagonal")
+@pytest.mark.parametrize(
+    "pos_inicial, pos_final"
+    [
+        ((0, 0),   (-1, -1)),      # 1
+        ((0, -5),  (0, -6)),       # 2
+        ((0, 100), (0, 99)),       # 3
+        ((-5, 0),  (-5, -1)),      # 4
+        ((-5, -5), (-5, -6)),      # 5
+        ((-5, 100), (-5, 99)),     # 6
+    ]
 
+)
+def test_posicion_deberia_mover_diagonal_derecha_arriba(pos_inicial, pos_final):
+    p= Posicion(pos_inicial, [0],pos_inicial[1] )
+    resultado = Posicion =p.new_posicion_diagonal_derecha_arriba()
+    esperado: Posicion = Posicion(pos_final[0], pos_final[1])
+    assert resultado == esperado
 
-def test_mover_abajo():
-    posicion_inicial = Posicion(2, 3)
-    nueva_posicion = posicion_inicial.new_posicion_abajo()
-    assert nueva_posicion.fila == 3
-    assert nueva_posicion.columna == 3
-    assert (nueva_posicion.fila, nueva_posicion.columna) == (3, 3)
+@pytest.mark.parametrize(
+    "tablero_esperado, ancho, alto"
+    [
+        ([[Luz.OFF]], 1, 1),
+        ([[Luz.OFF,Luz.OFF], [Luz.OFF,Luz.OFF]], 2, 2),
+        ([], 0, 0),
+        ([], -1, -1),
+        ([[]], 1, 0),
+        ([[Luz.OFF,Luz.OFF]], 2, 1),
+    ]       
+)
+def test_tablero_creacion_vacio(tablero_esperado, ancho, alto):
+    resultado = Tablero(ancho, alto).tablero
+    assert tablero_esperado == resultado
 
+@pytest.fixture
+def talero_manual():
+    return [
+        [Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF,]
+        [Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF,]
+        [Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF,]
+        [Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF,]
+        [Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF,]
+        [Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF, Luz.OFF,]
 
-def test_diagonal_inferior_derecha_desde_origen():
-    posicion_inicial = Posicion(0, 0)
-    nueva_posicion = posicion_inicial.new_posicion_abajo().new_posicion_derecha()
-    assert nueva_posicion.fila == 99  
-    assert nueva_posicion.columna == 99  
-    assert (nueva_posicion.fila, nueva_posicion.columna) == (
-        99, 99)  
+    ]
 
+def test_tablero_prender_apagar(tablero_grande:Tablero, tablero_manual: lis[list[int]])-> none:
+    tablero_grande.prender_apagar(Posicion(0, 0))
+    tablero_manual[0][0] = Luz.ON
+    assert tablero_grande.tablero== tablero_manual
 
-def test_diagonal_superior_izquierda_desde_origen():
-    posicion_inicial = Posicion(0, 0)
-    nueva_posicion = posicion_inicial.new_posicion_arriba().new_posicion_izquierda()
-    assert nueva_posicion.fila == 42 
-    assert nueva_posicion.columna == 42  
-    assert (nueva_posicion.fila, nueva_posicion.columna) == (42, 42)  
+    tablero_grande.prender_apagar(Posicion(0, 0))
+    tablero_manual[0][0] = Luz.OFF
+    assert tablero_grande.tablero== tablero_manual
 
+    tablero_grande.prender_apagar(Posicion(5, 5))
+    tablero_manual[5][5] = Luz.ON
+    assert tablero_grande.tablero== tablero_manual
 
-def test_diagonal_inferior_izquierda_desde_5_5():
-    posicion_inicial = Posicion(5, 5)
-    nueva_posicion = posicion_inicial.new_posicion_abajo().new_posicion_izquierda()
-    assert nueva_posicion.fila == 100  
-    assert nueva_posicion.columna == -100  
-    assert (nueva_posicion.fila, nueva_posicion.columna) == (0, 0)  
+    tablero_grande.prender_apagar(Posicion(5, 5))
+    tablero_manual[5][5] = Luz.OFF
+    assert tablero_grande.tablero== tablero_manual
 
+@pytest.mark.parametrize()
+def test_tablero_switch_Luz(tablero_grande, tablero_manual):
+    tablero_grande.switch_Luz = (Posicion(0, 0))
+    tablero_manual =[0][0]=Luz.ON
+    tablero_manual =[0][1]=Luz.ON
+    tablero_manual =[1][0]=Luz.ON
+    tablero_manual =[1][1]=Luz.ON
 
-def test_diagonal_superior_derecha_desde_3_3():
-    posicion_inicial = Posicion(3, 3)
-    nueva_posicion = posicion_inicial.new_posicion_arriba().new_posicion_derecha()
-    assert nueva_posicion.fila == 8 
-    assert nueva_posicion.columna == 8  
-    assert (nueva_posicion.fila, nueva_posicion.columna) == (8, 8)  
-
-
-def test_diagonal_superior_derecha_negativa():
-    posicion_inicial = Posicion(-2, -2)
-    nueva_posicion = posicion_inicial.new_posicion_arriba().new_posicion_derecha()
-    assert nueva_posicion.fila == 0  
-    assert nueva_posicion.columna == 0  
-    assert (nueva_posicion.fila, nueva_posicion.columna) == (0, 0) 
-
-
-def test_diagonal_doble_arriba_izquierda():
-    posicion_inicial = Posicion(1, 1)
-    nueva_posicion = posicion_inicial.new_posicion_arriba(
-    ).new_posicion_arriba().new_posicion_izquierda()
-    assert nueva_posicion.fila == 999 
-    assert nueva_posicion.columna == 999 
-    assert (nueva_posicion.fila, nueva_posicion.columna) == (999, 999) 
-
-
-def test_diagonal_fuera_de_rango():
-    posicion_inicial = Posicion(999, 999)
-    nueva_posicion = posicion_inicial.new_posicion_abajo().new_posicion_derecha()
-    assert nueva_posicion.fila == -1  
-    assert nueva_posicion.columna == -1 
-    assert (nueva_posicion.fila, nueva_posicion.columna) == (-1, -1) 
